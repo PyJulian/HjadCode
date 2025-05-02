2210f95185ec87fdcc1f1690030054325aa1abb2
Julia <worteljulian2@gmail.com>
2025-05-02 13:56:13 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,18 @@
-#!bin/bash
-# Simple installer, uninstaller soon 🙏
+#!/bin/bash
 
-mkdir -p ~/.local/bin && \
-curl -fsSL https://github.com/PyJulian/HjadCode/raw/refs/heads/main/releases/hjadcod1.bin -o ~/.local/bin/hjad && \
-chmod +x ~/.local/bin/hjad && \
-SHELL_RC="$([ -n \"$ZSH_VERSION\" ] && echo ~/.zshrc || echo ~/.bashrc)" && \
-grep -qxF 'export PATH="$HOME/.local/bin:$PATH"' "$SHELL_RC" || echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$SHELL_RC" && \
+echo -e "\033[38;5;111mHjad Installer (Info): Installing Hjad\033[0m"
+mkdir -p ~/.local/bin
+echo -e "\033[38;5;214mHjad Installer (Info): Downloading hjadcod1...\033[0m"
+curl -fsSL https://github.com/PyJulian/HjadCode/raw/refs/heads/main/releases/hjadcod1.bin -o ~/.local/bin/hjad
+echo -e "\033[38;5;214mHjad Installer (Info): Making hjad executable...\033[0m"
+chmod +x ~/.local/bin/hjad
+SHELL_RC="$([ -n \"$ZSH_VERSION\" ] && echo ~/.zshrc || echo ~/.bashrc)"
+if ! grep -qxF 'export PATH="$HOME/.local/bin:$PATH"' "$SHELL_RC"; then
+    echo -e "\033[38;5;214mHjad Installer (Info): Adding ~/.local/bin to your PATH...\033[0m"
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$SHELL_RC"
+else
+    echo -e "\033[38;5;46mHjad Installer (Success): ~/.local/bin is already in your PATH.\033[0m"
+fi
+echo -e "\033[38;5;214mHjad Installer (Info): Reloading your shell configuration...\033[0m"
 source "$SHELL_RC"
+echo -e "\033[38;5;34mHjad Installer (Success): Hjad successfully installed!\033[0m"
