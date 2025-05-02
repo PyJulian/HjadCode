6fb56779cbbf14689fc503c67e80e262bd7fffb1
Julia <worteljulian2@gmail.com>
2025-05-02 13:40:33 +0200

    Installer File

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # Simple installer, uninstaller soon 🙏
 
 mkdir -p ~/.local/bin && \
-curl -fsSL https://github.com/PyJulian/HjadCode/raw/refs/heads/main/releases/hjadcod1.sh -o ~/.local/bin/hjad && \
+curl -fsSL https://github.com/PyJulian/HjadCode/raw/refs/heads/main/releases/hjadcod1.bin -o ~/.local/bin/hjad && \
 chmod +x ~/.local/bin/hjad && \
 SHELL_RC="$([ -n \"$ZSH_VERSION\" ] && echo ~/.zshrc || echo ~/.bashrc)" && \
 grep -qxF 'export PATH="$HOME/.local/bin:$PATH"' "$SHELL_RC" || echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$SHELL_RC" && \
